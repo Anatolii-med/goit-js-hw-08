91bd7f3b3907611f4bc99928692d51dd03cd695b
Anatolii-med <anatolii@github.com>
2021-11-21 14:57:28 +0200

    all done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,23 @@
+const _ = require('lodash');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
+/*
+проверка при загрузке плеера на наличие сохраненных данных
+если есть - устанавливается начало воспроизведения
+*/
 player.on('play', function () {
-  console.log('played the video!');
+  if (localStorage.getItem('videoplayer-current-time')) {
+    const setTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+    player.setCurrentTime(setTime.seconds);
+  }
 });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+/*
+функция записи локальных даннных
+*/
+const playTime = function (time) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+};
+
+player.on('timeupdate', _.throttle(playTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+const _ = require('lodash');
+const form = document.querySelector('.feedback-form');
+// объект для хранения введенных данных
+let feedbackContent = {};
 
+//функция проверки localStorage и заполнения полей при положительном результате
+const onPageLoad = () => {
+  if (localStorage.getItem('feedback-form-state')) {
+    const loadlocalStorageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    form.email.value = loadlocalStorageData.email;
+    form.message.value = loadlocalStorageData.message;
+  }
+};
+onPageLoad();
+
+/* 
+функция отпраки формы с проверкой заполнения всех полей, 
+выведение в консоль объекта с введенными данными 
+удаление данных из localStorage
+*/
+const handleSubmit = function (event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(feedbackContent);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+};
+form.addEventListener('submit', handleSubmit);
+
+/*функция сохранения полей в localStorage
+без throttle работает отлично, 
+с throttle выдает ошибку в консоль и не сохраняет данные
+ */
+const saveInput = function (e) {
+  feedbackContent.email = e.currentTarget.email.value;
+  feedbackContent.message = e.currentTarget.message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackContent));
+};
+form.addEventListener('input', saveInput);
+// form.addEventListener('input', _.throttle(saveInput, 500));
