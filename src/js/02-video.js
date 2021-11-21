5937848debf756903612ffab79a92d53d8c9f16b
Anatolii-med <anatolii@github.com>
2021-11-21 16:53:04 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
@@ -6,12 +7,15 @@ const player = new Vimeo.Player(iframe);
 проверка при загрузке плеера на наличие сохраненных данных
 если есть - устанавливается начало воспроизведения
 */
-player.on('play', function () {
+
+const onPlayerLoad = function () {
   if (localStorage.getItem('videoplayer-current-time')) {
     const setTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
     player.setCurrentTime(setTime.seconds);
   }
-});
+};
+
+onPlayerLoad();
 
 /*
 функция записи локальных даннных
@@ -20,4 +24,19 @@ const playTime = function (time) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
 };
 
-player.on('timeupdate', _.throttle(playTime, 1000));
+player
+  .on('timeupdate', _.throttle(playTime, 1000))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
