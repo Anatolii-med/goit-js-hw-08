460f76cd2fff590bf99a18446cacf9c5da6518cd
Anatolii-med <anatolii@github.com>
2021-11-21 11:36:49 +0200

    1 done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,21 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+const createGalleryItemMarkup = function ({ preview, original, description }) {
+  return `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} title=${description}"/>
+</a>`;
+};
+
+const galleryItem = galleryItems.map(createGalleryItemMarkup).join('');
+galleryEl.insertAdjacentHTML('afterbegin', galleryItem);
+
+let gallery = new SimpleLightbox('.gallery a');
+
+gallery.on('show.simplelightbox', function () {
+  gallery.options.captionDelay = 250;
+});
